cd8344b42dcbec8e308365e1c39d616987465435
Danyang Wang <DAWA@simcorp.com>
2022-11-23 11:34:00 +0100

    Assignment 1

---
+++ CalcCurrentValue/CalcCurrentValue/CalcCurrentValue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcCurrentValue
+{
+    class CalcCurrentValue
+    {
+        public enum TransactionType
+        {
+            Buy,
+            Sell
+        }
+
+        public double CalcCV(double tradeprice, int nominal, TransactionType tp)
+        {
+             
+            return tp == TransactionType.Buy ? tradeprice * nominal : -1 * tradeprice * nominal;
+        }
+
+    }
+}
--- CalcCurrentValue/CalcCurrentValue/Program.cs
+++ CalcCurrentValue/CalcCurrentValue/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static CalcCurrentValue.CalcCurrentValue;
 
 namespace CalcCurrentValue
 {
@@ -10,6 +7,20 @@ namespace CalcCurrentValue
     {
         static void Main(string[] args)
         {
+            double price = 67.53d;
+            Console.WriteLine($"Current price is {price}.");
+            Console.WriteLine("Input the nominal of the trade: ");
+            int nominal = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the Transaction Type : Buy / Sell");
+            string transactionTpye = Console.ReadLine().Trim();
+
+            TransactionType trcType;
+            trcType = (TransactionType)Enum.Parse(typeof(TransactionType),transactionTpye,true);
+
+            var ccv = new CalcCurrentValue();
+            double result = ccv.CalcCV(price, nominal, trcType);
+            Console.WriteLine($"The Current Value is: {result}.");
+            Console.ReadKey();
         }
     }
 }
