793ff0d05f18a785ce374928ffc7331cb0ac9a0c
Danyang Wang <DAWA@simcorp.com>
2022-11-23 13:42:40 +0100

    Assignment 2

---
+++ CalcCurrentValue/CalcCurrentValue/CalcPL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcCurrentValue
+{
+    class CalcPL
+    {
+        public CalcPL()
+        {
+
+        }
+        public CalcPL(double tradeprice, double originalprice, int nominal)
+        {
+            TradePrice = tradeprice;
+            OriginalPrice = originalprice;
+            Nominal = nominal;
+        }
+        public double TradePrice { get; set; }
+        public double OriginalPrice { get; set; }
+        public int Nominal { get; private set; }
+
+        public double PL() => (TradePrice - OriginalPrice) * Nominal;
+
+    }
+}
--- CalcCurrentValue/CalcCurrentValue/Program.cs
+++ CalcCurrentValue/CalcCurrentValue/Program.cs
@@ -7,8 +7,8 @@ namespace CalcCurrentValue
     {
         static void Main(string[] args)
         {
-            double price = 67.53d;
-            Console.WriteLine($"Current price is {price}.");
+            double tradeprice = 67.53d;
+            Console.WriteLine($"Current price is {tradeprice}.");
             Console.WriteLine("Input the nominal of the trade: ");
             int nominal = int.Parse(Console.ReadLine());
             Console.WriteLine("Input the Transaction Type : Buy / Sell");
@@ -18,8 +18,18 @@ namespace CalcCurrentValue
             trcType = (TransactionType)Enum.Parse(typeof(TransactionType),transactionTpye,true);
 
             var ccv = new CalcCurrentValue();
-            double result = ccv.CalcCV(price, nominal, trcType);
+            double result = ccv.CalcCV(tradeprice, nominal, trcType);
             Console.WriteLine($"The Current Value is: {result}.");
+
+            if(trcType == TransactionType.Sell)
+            {
+                Console.WriteLine("Input the original price: ");
+                double originalprice = double.Parse(Console.ReadLine());
+                var pl = new CalcPL(tradeprice, originalprice, nominal);
+                var plResult = pl.PL();
+                Console.WriteLine($"The profit loss figure is: {plResult}");
+            }
+
             Console.ReadKey();
         }
     }
